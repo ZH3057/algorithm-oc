7dd67c6d9f2651e2c8c06d4f510e32de5758b6db
zhoujun <zhoujun@knowbox.cn>
2018-07-12 15:12:57 +0800

    project init

---
+++ Algorithm/Algorithm/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Algorithm
+//
+//  Created by Jun Zhou on 2018/7/12.
+//  Copyright © 2018年 Jun Zhou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
